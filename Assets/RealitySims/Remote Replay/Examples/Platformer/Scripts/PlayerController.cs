0c355a1093342929773d83f9e144d173ef6a04b1
Tobias Kristiansson <tobiaskristiansson@hotmail.com>
2024-01-30 22:47:07 +0100

    Support touch input to platformer game.

--- Assets/RealitySims/Remote Replay/Examples/Platformer/Scripts/PlayerController.cs
+++ Assets/RealitySims/Remote Replay/Examples/Platformer/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private Vector2 startTouchPosition, endTouchPosition;
+    private float horizontalInput;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,21 +20,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        HandleKeyboardInput();
+        HandleTouchInput();
         Move();
         Jump();
     }
 
+    private void HandleKeyboardInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); // Get keyboard input
+    }
+
+    private void HandleTouchInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startTouchPosition = touch.position;
+                    break;
+
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                    endTouchPosition = touch.position;
+                    horizontalInput = Mathf.Sign(endTouchPosition.x - startTouchPosition.x);
+                    break;
+
+                case TouchPhase.Ended:
+                    startTouchPosition = endTouchPosition = Vector2.zero;
+                    break;
+            }
+        }
+    }
+
     private void Move()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
     }
 
     private void Jump()
     {
         isGrounded = Physics2D.Raycast((Vector2)transform.position + Vector2.down, Vector2.down, 0.1f, groundLayer);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        // Check for jump input (keyboard or touch)
+        if ((Input.GetButtonDown("Jump") || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) && isGrounded)
         {
             rb.velocity = Vector2.up * jumpForce;
         }
