625ad19e37218072c935eb1040cc33216241247b
pavellangweil <pavellangweil@gmail.com>
2024-02-02 00:07:17 +0100

    Fix makepackage script and include UnityPackage

--- makepackage.bat
+++ makepackage.bat
@@ -1,9 +1,18 @@
-mkdir Assets\Build
-robocopy Assets\RealitySims\ Assets\Build\RealitySims\ /mir
-rmdir .\Assets\Build\RealitySims\Examples\IndieMarc\
-rmdir .\Assets\Build\RealitySims\Examples\JumpUp\
-rmdir .\Assets\Build\RealitySims\Examples\Tanks\
-del .\Assets\Build\RealitySims\Examples\IndieMarc.meta
-del .\Assets\Build\RealitySims\Examples\JumpUp.meta
-del .\Assets\Build\RealitySims\Examples\Tanks.meta
-"C:\Program Files\Unity\Hub\Editor\2021.3.21f1\Editor\Unity.exe" -quit -batchmode -nographics -projectPath "C:\Users\Pavel\rs-remote-replay" -exportPackage "Assets\Build" "C:\Users\Pavel\rs-remote-replay\UnityPackage\RemoteReplay.unitypackage"
+mkdir tmp
+
+move ".\Assets\RealitySims\Remote Replay\Examples\IndieMarc.meta" tmp\
+move ".\Assets\RealitySims\Remote Replay\Examples\JumpUp.meta" tmp\
+move ".\Assets\RealitySims\Remote Replay\Examples\Tanks.meta" tmp\
+
+move ".\Assets\RealitySims\Remote Replay\Examples\IndieMarc" tmp\
+move ".\Assets\RealitySims\Remote Replay\Examples\JumpUp" tmp\
+move ".\Assets\RealitySims\Remote Replay\Examples\Tanks" tmp\
+
+"C:\Program Files\Unity\Hub\Editor\2021.3.21f1\Editor\Unity.exe" -quit -batchmode -nographics -projectPath "C:\Users\Pavel\rs-remote-replay" -exportPackage "Assets\RealitySims" "C:\Users\Pavel\rs-remote-replay\UnityPackage\RemoteReplay.unitypackage"
+
+move "tmp\*" ".\Assets\RealitySims\Remote Replay\Examples\"
+move "tmp\IndieMarc" ".\Assets\RealitySims\Remote Replay\Examples\"
+move "tmp\JumpUp" ".\Assets\RealitySims\Remote Replay\Examples\"
+move "tmp\Tanks" ".\Assets\RealitySims\Remote Replay\Examples\"
+
+rmdir tmp
